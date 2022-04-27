4cacd6dacd46abcc2ffcc35aacb3e96c3fd60f92
Katherine_20 <mob1911494@gmail.com>
2022-04-27 11:31:49 +0300

    added project that find all numbers between a num and minus num

---
+++ Example005_between/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+Console.WriteLine("Введите любое натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int opposite_num = -num;
+
+Console.WriteLine("между числом " + opposite_num + " и" + num + " находятся следующие целые числа: ");
+do
+{
+    Console.Write(opposite_num + ", ");
+    opposite_num = opposite_num + 1;
+} while (opposite_num < num);
+
+if (opposite_num == num) Console.WriteLine(" " + opposite_num + ".");
