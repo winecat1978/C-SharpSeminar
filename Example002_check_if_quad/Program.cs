0a148ad61310620abfc1789921b4d9d0dc57893b
Katherine_20 <mob1911494@gmail.com>
2022-04-27 10:57:25 +0300

    added project that checks if one num is a result of quadr of another

---
+++ Example002_check_if_quad/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+Console.WriteLine("введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2*num2) 
+{ Console.WriteLine(num1 + " является квадратом числа " + num2);
+}
+else 
+Console.WriteLine("Вы ввели какой-то бред");
